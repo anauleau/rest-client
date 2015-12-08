1b4cc23f488e4a50345cfa257837a5c7e8fc36d5
Andreas Nauleau <anauleau@gmail.com>
2015-12-07 23:32:11 -0800

    scaffold application with yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name restClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the restClientApp
+ */
+angular.module('restClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
